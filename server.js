6a076db971664e0af1f63f194a2bbcb539e79dd8
MKS-Student <mks.student@hackreactor.com>
2016-10-10 12:07:21 -0700

    getting deploy to work

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.listen(port);
